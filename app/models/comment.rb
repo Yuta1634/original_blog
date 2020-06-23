dcbca20f9d2a2ac0db8ff260b98121fa048b2240
Yuta1634 <yuta1994year@gmail.com>
2020-06-23 11:49:04 +0900

    fix comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  validates :text, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :text, presence: true, format: { with: /\p{Hiragana}/, \
-  message: 'には日本語を含めてください。(Text must contain hiragana.)' }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :text, presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/, 
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
