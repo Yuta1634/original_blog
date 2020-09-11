7a558c744acbc98287ba37afeabb9810d2af1161
Yuta1634 <yuta1994year@gmail.com>
2020-09-11 21:27:46 +0900

    create

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+# Capistranoのログの表示に利用する
+set :application, 'original_blog'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:Yuta1634/original_blog.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['1fd3fdee144eee3f84ca0e891c60b7b23a70bc5c3652ca876bec1054b3f15b805f98da3c0bb5b832cdf814b0639e653754347b9e23dfc4ed76279b590e90c1c4'],
+                  keys: ['<ローカルPCのEC2インスタンスのSSH鍵(pem)へのパス（例：~/.ssh/key_pem.pem）>'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
