de70e4db752afe1e2fdb7d3630051746add82496
Yuta1634 <yuta1994year@gmail.com>
2020-09-15 18:49:29 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'microblog'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/microblog'
 end
